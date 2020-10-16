219d83cf4036ab86d99162dd74a8fb5f27af988b
Andres M <estfabiber2@hotmail.com>
2020-10-16 17:20:34 -0500

    Update conexion.php
    Delete name of database by default

--- admin/clases/conexion.php
+++ admin/clases/conexion.php
@@ -2,7 +2,7 @@
 
 function conectar_db(){
     try {
-        $conexion = new PDO('mysql:host=127.0.0.1;dbname=virtual_dbcei;charset=UTF8;','root','');
+        $conexion = new PDO('mysql:host=127.0.0.1;dbname=;charset=UTF8;','root','');
         return $conexion;
     } catch (PDOException $e) {
         return false;
