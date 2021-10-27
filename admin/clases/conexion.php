5d6ca702108d90e946619bfbfbdf494b7addadfb
Andres Mora <estfabiber2@hotmail.com>
2021-10-27 11:09:02 -0500

    Update versions of class, functions, procedures

--- admin/clases/conexion.php
+++ admin/clases/conexion.php
@@ -2,7 +2,7 @@
 
 function conectar_db(){
     try {
-        $conexion = new PDO('mysql:host=;dbname=;charset=UTF8;','','');
+        $conexion = new PDO('mysql:host=localhost;dbname=virtual_dbcei;charset=UTF8;','root','root');
         return $conexion;
     } catch (PDOException $e) {
         return false;
